ae061d41e5377fcbdc88cbe517c485a4370cc8d6
陈晓 <shawnchen@chenxiaodeMacBook-Pro.local>
2020-03-31 10:29:58 +0800

    Add app for lists, with deliberately failing unit test

---
+++ functional_tests.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):    # (1)
+    
+    def setUp(self):    # (3)
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self): # (3)
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  # (2)
+        # Edith has heard about a cool new online to-do lists
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-do', self.browser.title)  # (4)
+        self.fail('Finish the test!')   # (5)
+    
+if __name__ == '__main__':  # (6)
+    unittest.main(warnings='ignore')    # (7)
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# when she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effecct.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+# browser.quit()
