0d16c58efe55706cc44514041469c0727e27ee17
ChenXiao <892230084@qq.com>
2020-04-21 11:07:42 +0800

    Change func and views and test and home

--- lists/tests.py
+++ lists/tests.py
@@ -4,28 +4,32 @@ from django.http import HttpRequest
 
 from lists.views import home_page   # (2)
 
+
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')    # (1)
-        self.assertEqual(found.func, home_page) # (1)
-    
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/') # 1
-
-        html = response.content.decode('utf8')  # 2
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html')  # 3
-        
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-        
-        
-        
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+    # def test_root_url_resolve_to_home_page_view(self):
+    #     found = resolve('/')    # (1)
+    #     self.assertEqual(found.func, home_page) # (1)
+
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/') # 1
+
+    #     html = response.content.decode('utf8')  # 2
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+
+    #     self.assertTemplateUsed(response, 'home.html')  # 3
+
         # **************重构****************
         # request = HttpRequest() # 1
         # response = home_page(request)   # 2
