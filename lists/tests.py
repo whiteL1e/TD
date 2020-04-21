ffa102186f09ae335f0b2fabd11d1c7a443ae489
ChenXiao <892230084@qq.com>
2020-04-21 22:45:05 +0800

    Change the functional_test.py and add the ItemModelTest Class in tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from lists.models import Item
 
 from lists.views import home_page   # (2)
 
@@ -16,27 +17,47 @@ class HomePageTest(TestCase):
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
-    # def test_root_url_resolve_to_home_page_view(self):
-    #     found = resolve('/')    # (1)
-    #     self.assertEqual(found.func, home_page) # (1)
 
-    # def test_home_page_returns_correct_html(self):
-    #     response = self.client.get('/') # 1
+class ItemModelTest(TestCase):
 
-    #     html = response.content.decode('utf8')  # 2
-    #     self.assertTrue(html.startswith('<html>'))
-    #     self.assertIn('<title>To-Do lists</title>', html)
-    #     self.assertTrue(html.strip().endswith('</html>'))
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-    #     self.assertTemplateUsed(response, 'home.html')  # 3
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
 
-        # **************重构****************
-        # request = HttpRequest() # 1
-        # response = home_page(request)   # 2
-        # html = response.content.decode('utf8')  # 3
-        # self.assertTrue(html.startswith('<html>'))  # 4
-        # self.assertIn('<title>To-Do lists</title>', html)   # 5
-        # self.assertTrue(html.endswith('</html>'))    # 4
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+
+# def test_root_url_resolve_to_home_page_view(self):
+#     found = resolve('/')    # (1)
+#     self.assertEqual(found.func, home_page) # (1)
+
+# def test_home_page_returns_correct_html(self):
+#     response = self.client.get('/') # 1
+
+#     html = response.content.decode('utf8')  # 2
+#     self.assertTrue(html.startswith('<html>'))
+#     self.assertIn('<title>To-Do lists</title>', html)
+#     self.assertTrue(html.strip().endswith('</html>'))
+
+#     self.assertTemplateUsed(response, 'home.html')  # 3
+
+# **************重构****************
+# request = HttpRequest() # 1
+# response = home_page(request)   # 2
+# html = response.content.decode('utf8')  # 3
+# self.assertTrue(html.startswith('<html>'))  # 4
+# self.assertIn('<title>To-Do lists</title>', html)   # 5
+# self.assertTrue(html.endswith('</html>'))    # 4
 
 # **************1*****************
 # class SmokeTest(TestCase):
