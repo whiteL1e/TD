67de108eb5b7993f3b010fbd5fb19e70d0017c50
ChenXiao <892230084@qq.com>
2020-04-28 00:15:04 +0800

    Correct the functional_test.py

--- functional_tests.py
+++ functional_tests.py
@@ -26,6 +26,14 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1:Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
