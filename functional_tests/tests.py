cebdfe6e839c53ebd6aeaddfb3cfc39b4c618b18
CDC-Melo <43125330+CDC-Melo@users.noreply.github.com>
2020-05-30 17:38:54 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -6,7 +6,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         self.browser.get(self.live_server_url)
